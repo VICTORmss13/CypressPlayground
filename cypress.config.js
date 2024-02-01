15f9bb5720a281f8a84ff787de33916182c15386
Victor Mendes <victormendes13@gmail.com>
2024-01-31 23:22:58 -0300

    Organizing configuration files

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  pluginsFile: false,
+  viewportHeight: 880,
+  viewportWidth: 1280,
+  e2e: {
+    setupNodeEvents(on, config) {},
+  },
+})
