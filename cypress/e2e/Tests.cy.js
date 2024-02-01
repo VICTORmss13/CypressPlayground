4108d20e36a4288866766ce72eefb8651b24c3ea
Victor Mendes <victormendes13@gmail.com>
2024-02-01 13:16:48 -0300

    Starting the tests

--- cypress/e2e/Tests.cy.js
+++ cypress/e2e/Tests.cy.js
@@ -1,7 +1,40 @@
 describe('Cypress Playground', function() {
-  it('Acesso ao site', function() {
-    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
-
-    cy.get('button').click()
+  beforeEach( function() {
+    it('Acesso ao site', function() {
+      cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+    })
   })
+
+  it('Verificando .click()', function() {
+    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+    cy.get('button[type="submit"]').click()
+    // could be: cy.contains('button','Subscribe').click() 
+  })
+
+  it('Verificando .type()', function() {
+    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+    cy.get('textarea[placeholder="Joe Doe"]')
+      .type('Testando o campo "Sign here"')
+  })
+
+  it('Verificando .check() and .uncheck()', function() {
+    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+    cy.get('textarea[placeholder="Jane Doe"]')
+      .type('Tentando o campo Sign Here Jane Doe')
+    cy.get('input[type="checkbox"]')
+      .check()
+      .uncheck()
+  })
+
+  it('Verificando radio input fields', function() {
+    cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+    cy.get('#off')
+    .check()
+    // could also be: cy.get('input[type="radio"]').check()
+  })
+
+  it('Verificando .select()', function() {
+    cy.visit
+  })
+  
 })
