94dbb2b9ffc8b86339611af98bdb91564329b8f1
Victor Mendes <victormendes13@gmail.com>
2024-02-01 01:43:14 -0300

    cy.get learning

--- cypress/e2e/Tests.cy.js
+++ cypress/e2e/Tests.cy.js
@@ -1,5 +1,7 @@
 describe('Cypress Playground', function() {
   it('Acesso ao site', function() {
     cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+
+    cy.get('button').click()
   })
 })
